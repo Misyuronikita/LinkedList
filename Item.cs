5e4e47cf20df009b675e7e0dfdefed771586e0ea
Nikita <104940451+Misyuronikita@users.noreply.github.com>
2022-09-20 17:42:32 +0300

    Add one item of linked list

---
+++ Item.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LinkedListByMisyuro
+{
+    /// <summary>
+    /// Ячейка списка
+    /// </summary>
+    public class Item<T>
+    {
+
+        private T data = default(T);
+        /// <summary>
+        /// Данные хранимые в ячейке списка
+        /// </summary>
+        public T Data
+        {
+            get { return data; }
+            set
+            {
+                if (data != null)
+                {
+                    data = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Следующая ячейка списка
+        /// </summary>
+        public Item<T> Next { get; set; }
+
+        public Item(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
